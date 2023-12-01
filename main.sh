f6a24e0c19215234cdaac5362c2e35abacfcaec6
Kurolycka <150124061+Kurolycka@users.noreply.github.com>
2023-12-01 14:56:08 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 
 if [ "$1" == "-h" ] || [ "$1" == "-help" ]; then #Affichage de l'help de base, -h et -help.
     echo -e "\n $(basename $0) -- Programme qui indexe les intensités par fenêtres de longueurs d'ondes selon une taille de pas que vous nous donnez. Permet aussi d'afficher un graphe des intensités en fonction des longueurs d'ondes pour un intervalle fourni.
-    \n Afin de ne pas noyer l'utilisateur sous les informations l'aide est divisée en catégorie avec plusieurs options. Choisissez celle correspondant aux informations que vous recherchez. 
+    \n Afin de ne pas noyer l'utilisateur sous les informations l'aide est divisée en catégories avec plusieurs options. Choisissez celle correspondant aux informations que vous recherchez. 
     
     Options : 
     
@@ -12,7 +12,10 @@ if [ "$1" == "-h" ] || [ "$1" == "-help" ]; then #Affichage de l'help de base, -
     -help : Affiche également cette aide.
     -hp : Aide dédiée aux paramètres.
     -hg : Aide dédiée au graphique.
-    -ht : Aide dédiée au traitement des données. \n"
+    -ht : Aide dédiée au traitement des données. 
+
+    Informations plus détaillées conçernant ce programme sur le git correspondant : https://github.com/Kurolycka/Projet-Systeme-M1.git"
+
     exit #Sortie du programme.
 
 elif [ "$1" == "-hp" ]; then #Affichage d'une des ramifications de l'help qui est -hp pour les paramètres.
@@ -22,27 +25,27 @@ elif [ "$1" == "-hp" ]; then #Affichage d'une des ramifications de l'help qui es
     Le chemin du fichier doit forcément correspondre à un fichier régulier existant ayant l'extension csv ou txt. Il ne doit également pas être vide.
     Exemple : /mnt/c/Users/33782/Desktop/Donnees.txt
     
-    \e[1;36;4mNuméro de la colonne contenant les longueurs d'ondes (colonne_longueur_onde) :\e[0m  Doit être un nombre entier strictement positif et ne doit pas être supérieur au nombre de colonnes contenues dans votre fichier.
+    \e[1;36;4mNuméro de la colonne contenant les longueurs d'onde (colonne_longueur_onde) :\e[0m  Doit être un nombre entier strictement positif et ne doit pas être supérieur au nombre de colonnes contenues dans votre fichier.
     La première colonne de votre fichier a le numéro 1 et non le numéro 0. 
     
-    \e[1;36;4mNuméro de la colonne contenant les intensitées (colonne_intensitees) :\e[0m  Doit être un nombre entier strictement positif et ne doit pas être supérieur au nombre de colonnes contenues dans votre fichier.
+    \e[1;36;4mNuméro de la colonne contenant les intensités (colonne_intensitees) :\e[0m  Doit être un nombre entier strictement positif et ne doit pas être supérieur au nombre de colonnes contenues dans votre fichier.
     La première colonne de votre fichier a le numéro 1 et non le numéro 0. 
     
     \e[1;36;4mTaille du pas (taille_fenetre) :\e[0m  Doit être un nombre strictement positif. Cela peut être un entier comme un float, cependant si vous voulez un float rentrez par exemple 12.6 et non 12,6. 
 
-    \e[1;36;4mUnité de vos longueurs d'ondes (unite_long_onde) :\e[0m  Il n'y a pas de conditions sur ce paramètre. Il servira à spécifier l'abscisse de votre graphe.
+    \e[1;36;4mUnité de vos longueurs d'onde (unite_long_onde) :\e[0m  Il n'y a pas de conditions sur ce paramètre. Il servira à spécifier l'abscisse de votre graphe.
 
     \e[1;36;4mUnité de vos intensités (unite_intens) :\e[0m  Il n'y a pas de conditions sur ce paramètre. Il servira à spécifier l'ordonnée de votre graphe.
 
     \e[1;36;4mLongueur d'onde de début d'intervalle (long_deb) :\e[0m  Doit être un nombre positif (0 autorisé). Il peut être un float comme un entier mais si vous voulez un float rentrez par exemple 128.3 et non 128,3. 
-    Renseignez la dans l'unité que vous avez choisit pour le paramètre 'Unité de vos longueurs d'ondes'.
+    Renseignez la dans l'unité que vous avez choisie pour le paramètre 'Unité de vos longueurs d'ondes'.
 
     \e[1;36;4mLongueur d'onde de fin d'intervalle (long_fin) :\e[0m  Doit être un nombe strictement positif (0 non autorisé). Il peut être un float comme un entier mais si vous voulez un float rentrez par exemple 128.3 et non 128,3.
-    Renseignez la dans l'unité que vous avez choisit pour le paramètre 'Unité de vos longueurs d'ondes'.
+    Renseignez la dans l'unité que vous avez choisit pour le paramètre 'Unité de vos longueurs d'onde'.
 
     \e[1;36;4m Le séparateur (sep) :\e[0m La chaîne de caractère entre deux de vos valeurs numériques dans votre fichier.
 
-    \e[1;32mPour certains paramètres vous aurez de nouvelles chances de rentrer le bon paramètre sans quitter le programme si vous vous êtes trompés.\e[0m
+    \e[1;32mPour certains paramètres vous aurez de nouvelles chances de rentrer le bon paramètre sans quitter le programme si vous vous êtes trompé.\e[0m
     "
     exit #Sortie du programme..
 
@@ -50,10 +53,10 @@ elif [ "$1" == "-hg" ]; then #Affichage d'une des ramifications de l'help qui es
     echo -e "\e[1;32mAide dédiée au graphique. Pour revenir à l'aide principale utilisez l'option -h ou bien l'option -help.\e[0m
     
     \e[1;36;4mFonctionnement du graphe :\e[0m Vous fournissez une valeur de début d'intervalle pour le graphe et une valeur de fin d'intervalle pour le graphe et vous avez précédemment défini un pas.
-    Tout d'abord vos données de longueurs d'ondes et leurs intensités correspondantes sont séparées en intervalles selon le pas choisi. Si par exemple vous avez des données entre 300 et 400 nm et que vous choisissez un pas de 10 vous aurez des intervalles tels que [300,310[;[310, 320[ ... [380,390[;[390,400].
+    Tout d'abord vos données de longueurs d'onde et leurs intensités correspondantes sont séparées en intervalles selon le pas choisi. Si par exemple vous avez des données entre 300 et 400 nm et que vous choisissez un pas de 10 vous aurez des intervalles tels que [300,310[;[310, 320[ ... [380,390[;[390,400].
     Ensuite le programme regarde dans quel intervalle se situe votre valeur de début d'intervalle pour le graphe et garde en mémoire cette intervalle.
     Il fait ensuite de même pour votre valeur de fin d'intervalle pour le graphe.
-    Ce qui sera au final affiché sur le graphe sera les intervalles dans lesquels sont contenus vos valeurs de début et de fin d'intervalle pour le graphe ainsi que tous les intervalles entre.
+    Ce qui sera au final affiché sur le graphe seront les intervalles dans lesquels sont contenues vos valeurs de début et de fin d'intervalle pour le graphe ainsi que tous les intervalles entre.
     
     \e[1;36;4mVotre valeur de début d'intervalle de graphe est plus petite que la plus petite donnée de longueur d'onde de votre fichier :\e[0m Alors votre graphe commencera à la plus petite donnée de longueur d'onde de votre fichier.
 
@@ -72,9 +75,9 @@ elif [ "$1" == "-ht" ]; then #Affichage d'une des ramifications de l'help qui es
 
     Voici quelques informations concernant le traitement des données qui ne semblent probablement pas évidentes : 
 
-    \e[1;36;4mPremière ligne :\e[0m Votre première ligne doit obligatoirement correspondre aux noms de vos colonnes. Si vous avez vos valeurs dès la première ligne, la colonne ne traitera pas cette première ligne. Ainsi si vous n'avez qu'une ligne dans votre fichier, le programme considère que ce sont les noms de vos colonnes et que vous n'avez donc pas de données. Le fichier ne sera donc pas traité.
-
     \e[1;36;4mValeur de longueur d'onde sans valeur d'intensité ou vice-versa :\e[0m Impossible. Si c'est le cas l'absence de valeur sera considéré comme un 'null' et vous ne remplirez pas la condition d'avoir des données qui sont des nombres entiers ou décimaux. Vous aurez donc un message d'erreur et le programme ne s'exécutera pas.
+
+    \e[1;36;4mCommentaires :\e[0m Il vous est possible de mettre des commentaires dans votre fichier qui seront affichés lors de l'exécution du programme. Cependant ces derniers doivent obligatoirement être précédés par un #.
 
     \n"
     exit #Sortie du programme.
@@ -154,7 +157,7 @@ done
 #-----------------------Récupération et vérification de la colonne des intensitées------------------------------
 
 
-read -p "Entrez le numéro de la colonne qui contient les intensitées: " colonne_intensitees #Je récupère la colonne des intensitées.
+read -p "Entrez le numéro de la colonne qui contient les intensités: " colonne_intensitees #Je récupère la colonne des intensitées.
 
 somme_ci=0 #Pour savoir combien de fois l'utilisateur a déjà donné une valeur invalide.
 
