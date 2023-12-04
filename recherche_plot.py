1c10af1d478d05cdd3cc270190f1cef699760ba8
Kurolycka <150124061+Kurolycka@users.noreply.github.com>
2023-12-04 15:43:53 +0100

    Update recherche_plot.py

--- recherche_plot.py
+++ recherche_plot.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 #-----------------------Je récupère les paramètres et la liste utiles au programme------------------------------
 
-
+liste_clefs=inten.liste_clefs
 ma_liste=inten.liste_retour
 long_deb=float(sys.argv[5])
 long_fin=float(sys.argv[6])
@@ -30,39 +30,27 @@ y_inten=[]
 
 #-----------------------Je cherche les intervalles à afficher------------------------------
 
-
 #Mes valeurs par défaut des indices d'intervalles à afficher sont le premier et le dernier :
 deb_ind=0
 fin_ind=len(ma_liste)-1 
 
 #Je cherche l'intervalle du dictionnaire dans lequel se trouve la longueur d'onde de début d'intervalle pour le graphe et j'en récupère l'indice dans deb_ind.:
 for i in range(0,len(ma_liste)-1):
-    if ma_liste[i] and ma_liste[i][0]: #Je regarde si il y a des valeurs dans cet intervalle.
-        if len(ma_liste[i][0])==1: #Je regarde si l'intervalle n'a qu'une seule valeur.
-            if long_deb<=ma_liste[i][0][0]:
-                deb_ind=i
-                break
-        else:
-            if long_deb >= ma_liste[i][0][0] and long_deb < ma_liste[i][0][-1] :
-                deb_ind = i 
-                break
+    if long_deb >= liste_clefs[i][0] and long_deb < liste_clefs[i][-1]:
+        deb_ind=i
+        break 
+
 
 #Je cherche l'intervalle du dictionnaire dans lequel se trouve la longueur d'onde de fin d'intervalle pour le graphe et j'en récupère l'indice dans deb_fin.:
 for j in range(deb_ind, len(ma_liste)):
-    if ma_liste[j] and ma_liste[j][0]: #Je regarde si il y a des valeurs dans cet intervalle.
-        if len(ma_liste[j][0])==1: #Je regarde si l'intervalle n'a qu'une seule valeur.
-            if ma_liste[j][0][0]>=long_fin:
-                fin_ind=j
+    if j == len(ma_liste)-1: #Je regarde si c'est le dernier intervalle.
+        if long_fin >= liste_clefs[j][0] and long_fin <= liste_clefs[j][-1]:
+                fin_ind = j
                 break
-        else :
-            if j == len(ma_liste)-1: #Je regarde si c'est le dernier intervalle.
-                if long_fin >= ma_liste[j][0][0] and long_fin <= ma_liste[j][0][-1]:
-                    fin_ind = j
-                    break
-            else:
-                if long_fin >= ma_liste[j][0][0] and long_fin < ma_liste[j][0][-1]:
-                    fin_ind = j
-                    break
+    else:
+        if long_fin >= liste_clefs[j][0] and long_fin < liste_clefs[j][-1]:
+            fin_ind = j
+            break
 
 
 
@@ -81,8 +69,8 @@ for i in range(deb_ind,fin_ind+1):
 
 option_affichage=str(input("\033[1;32mVoulez vous afficher les données (minimum, maximum...) correspondant au graphe ? Si oui tapez Y sinon tapez N: \033[0m"))#Je lui demande si il veut que j'affiche les données.
 while (option_affichage !="Y" and option_affichage != "N"): #Si il me répond pas la bonne chose.
-    print("La valeur rentrée n'est pas correcte.") #Je lui demande en boucle une réponse au bon format et lui affiche un message d'erreur.
-    option_affichage=str(input("Voulez vous afficher les données (minimum, maximum...) correspondant au graphe ? Si oui tapez Y sinon tapez N: "))
+    print("\033[1;91mLa valeur rentrée n'est pas correcte.\033[0m") #Je lui demande en boucle une réponse au bon format et lui affiche un message d'erreur.
+    option_affichage=str(input("\033[1;32mVoulez vous afficher les données (minimum, maximum...) correspondant au graphe ? Si oui tapez Y sinon tapez N: \033[0m"))
 if option_affichage=="Y": #Je lui affiche les données si il répond positivement. 
     print("Valeur max de longueur d'onde: "+str(max(x_long))+" "+unite_long_onde+
           "\n Valeur min de longueur d'onde: "+str(min(x_long))+" "+unite_long_onde+
